ca11e4669c31e60e4c167478673f9b29e9303719
Ankit Anand <ankitanand@Ankits-MacBook-Pro.local>
2019-03-13 12:57:28 +0530

    second commit

---
+++ assets/js/animation3.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 600) {        // If page is scrolled more than 50px
+        $('#scrollUp').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#scrollUp').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#scrollUp').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
